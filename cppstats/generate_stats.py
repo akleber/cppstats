1d24eb2f092be06f89e5912daf8d3c01c9d30092
Andreas Kleber <andreas@drosselweg7a.de>
2016-03-21 11:05:43 +0100

    (wip)
    * first support for compilation database
    * logging
    * debugging output

--- cppstats/generate_stats.py
+++ cppstats/generate_stats.py
@@ -1,6 +1,13 @@
 import clang.cindex
+import json
+import os
+import re
+import logging
 
 #Stats = namedtuple('Stats', 'classes_count, method_count, member_count')
+
+#logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 def get_annotations(node):
     return [c.displayname for c in node.get_children()
@@ -18,6 +25,15 @@ class Class(object):
         self.public_methods = []
         self.private_methods = []
         self.annotations = get_annotations(cursor)
+
+        if cursor.spelling == "Reader":
+            print ("spelling: %s" % (cursor.spelling,))
+            print ("get_usr: %s" % (cursor.get_usr(),))
+            print ("displayname: %s" % (cursor.displayname,))
+            print ("mangled_name: %s" % (cursor.mangled_name,))
+            print ("location: %s" % (cursor.location,))
+            #print ("extend %s" % (cursor.extent,))
+
 
         for c in cursor.get_children():
             if (c.kind == clang.cindex.CursorKind.CXX_METHOD): 
@@ -54,5 +70,41 @@ def process_tu(translation_unit):
     print("classes: %i, public methods: %i, private methods: %i, annotations count: %i" % (len(classes),sum_public_methods,sum_private_methods,sum_annotations))
 
     print(*classes, sep='\n')
+
+
+def process_source_file(file, args):
+    logging.debug('process_source_file: %s' % (file,))
+
+    # remove outfile
+    outfile_regex = re.compile(r"-o \S*", re.IGNORECASE)
+    args = outfile_regex.sub('', args)
+
+    #remove sourcefile
+    sourcefile_regex = re.compile(r"-c \S*", re.IGNORECASE)
+    args = sourcefile_regex.sub('', args)
+
+    args_list = args.split(' ')
+
+    #remove empty entries
+    args_list = [x for x in args_list if x]
+
+    index = clang.cindex.Index.create()
+    tu = index.parse(file, args_list)
+
+    process_tu(tu)
+
+
+def process_compile_commands_db(file):
+    with open(file) as db_file:    
+        data = json.load(db_file)
+
+    #for entry in data:
+    #    process_source_file(entry["file"], entry["command"])
+
+    process_source_file(data[0]["file"], data[0]["command"])
+
+if __name__ == '__main__':
+    db_file = os.path.join(os.path.dirname(__file__), '..', 'tests', 'jsoncpp', 'build', 'compile_commands.json')
+    process_compile_commands_db(db_file)
 
 
