69b40b234d16cff1b16876e44fc983eeed0d5d89
Andreas Kleber <andreas@drosselweg7a.de>
2016-03-23 10:54:28 +0100

    adds statistics output, first meaningful output

--- cppstats/generate_stats.py
+++ cppstats/generate_stats.py
@@ -9,6 +9,23 @@ import logging
 #logging.basicConfig(filename='example.log',level=logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
 
+def print_statistics(classes):
+    class_count = len(classes)
+    public_pethod_count = 0
+    private_method_count = 0
+
+    for c in classes:
+        for m in c.methods:
+            if (m.access == clang.cindex.AccessSpecifier.PUBLIC):
+                public_pethod_count += 1
+            elif (m.access == clang.cindex.AccessSpecifier.PRIVATE):
+                private_method_count += 1
+
+    logging.info('classes        : %i', class_count)
+    logging.info('public_methods : %i', public_pethod_count)
+    logging.info('private_methods: %i', private_method_count)
+
+
 def get_annotations(node):
     return [c.displayname for c in node.get_children()
             if c.kind == clang.cindex.CursorKind.ANNOTATE_ATTR]
@@ -24,28 +41,19 @@ class Class(object):
         self.name = cursor.spelling
         self.usr = cursor.get_usr()
 
-        self.public_methods = []
-        self.private_methods = []
+        self.methods = []
         #self.annotations = get_annotations(cursor)
 
-        #if cursor.spelling == "Reader":
-        #    print ("spelling: %s" % (cursor.spelling,))
-        #    print ("get_usr: %s" % (cursor.get_usr(),))
-        #    print ("displayname: %s" % (cursor.displayname,))
-        #    print ("mangled_name: %s" % (cursor.mangled_name,))
-        #    print ("location: %s" % (cursor.location,))
-        #    print ("extend %s" % (cursor.extent,))
-
         logging.debug('created Class for: %s', cursor.spelling)
-
 
         for c in cursor.get_children():
             if (c.kind == clang.cindex.CursorKind.CXX_METHOD): 
                 m = Method(c)
-                if (c.access_specifier == clang.cindex.AccessSpecifier.PUBLIC):
-                    self.public_methods.append(m)
-                elif (c.access_specifier == clang.cindex.AccessSpecifier.PRIVATE):
-                    self.private_methods.append(m)
+                self.methods.append(m)
+
+        dummy_classes = set()
+        dummy_classes.add(self)
+        print_statistics(dummy_classes)
 
     def __repr__(self):
         return "%s" % (self.usr, )
@@ -57,7 +65,7 @@ class Class(object):
     def __hash__(self):
         return hash(self.usr)
 
-def get_classes(cursor, filename):
+def collect_classes(cursor, filename):
     classes = set()
 
     (drive, tail) = os.path.splitdrive(filename)
@@ -73,7 +81,7 @@ def get_classes(cursor, filename):
                 a_class = Class(c)
                 classes.add(a_class)
             elif c.kind == clang.cindex.CursorKind.NAMESPACE:
-                child_classes = get_classes(c, filename)
+                child_classes = collect_classes(c, filename)
                 classes.update(child_classes)
 
     return classes 
@@ -83,14 +91,15 @@ def process_tu(translation_unit):
     logging.debug('process_tu: %s', tu_filename)
 
     cursor = translation_unit.cursor
-    classes = get_classes(cursor, tu_filename)
+    classes = collect_classes(cursor, tu_filename)
 
     #sum_public_methods = sum([len(x.public_methods) for x in classes])
     #sum_private_methods = sum([len(x.private_methods) for x in classes])
     #sum_annotations = sum([len(x.annotations) for x in classes])
     #print("classes: %i, public methods: %i, private methods: %i, annotations count: %i" % (len(classes),sum_public_methods,sum_private_methods,sum_annotations))
     
-    print(*classes, sep='\n')
+    #print(*classes, sep='\n')
+    return classes
 
 
 def process_source_file(file, args):
@@ -112,7 +121,9 @@ def process_source_file(file, args):
     index = clang.cindex.Index.create()
     tu = index.parse(file, args_list)
 
-    process_tu(tu)
+    classes = process_tu(tu)
+
+    print_statistics(classes)
 
 
 def process_compile_commands_db(file):
@@ -121,8 +132,8 @@ def process_compile_commands_db(file):
     with open(file) as db_file:    
         data = json.load(db_file)
 
-    #for entry in data:
-    #    process_source_file(entry["file"], entry["command"])
+    for entry in data:
+        process_source_file(entry["file"], entry["command"])
 
     process_source_file(data[0]["file"], data[0]["command"])
 
@@ -130,7 +141,7 @@ if __name__ == '__main__':
     #db_file = os.path.join(os.path.dirname(__file__), '..', 'tests', 'jsoncpp', 'build', 'compile_commands.json')
     #process_compile_commands_db(db_file)
 
-    source_file = os.path.join(os.path.dirname(__file__), '..', 'tests', 'jsoncpp', 'include', 'json', 'reader.h')
+    source_file = os.path.join(os.path.dirname(__file__), '..', 'tests', 'jsoncpp', 'include', 'json', 'writer.h')
     #source_file = os.path.join(os.path.dirname(__file__), '..', 'tests', 'jsoncpp', 'src', 'lib_json', 'json_reader.cpp')
     process_source_file(source_file, "-I/Users/andreaslangs/Shares/NAS/Dev/python/cppstats/tests/jsoncpp/include -I/Users/andreaslangs/Shares/NAS/Dev/python/cppstats/tests/jsoncpp/src/lib_json/../../include   -std=c++11 -Wall -Wconversion -Wshadow -Werror=conversion -Werror=sign-compare -O3 -DNDEBUG -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk")
 
